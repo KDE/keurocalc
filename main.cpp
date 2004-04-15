b3bd54ef42837fb5a4673ad60228946ef0d2e436
Éric Bischoff <bischoff@kde.org>
2004-04-15 12:14:12 +0000

    CVS_SILENT Changed logo and added splash screen. Correct icons are still missing.
    svn path=/trunk/kdeextragear-3/keurocalc/; revision=303921

--- main.cpp
+++ main.cpp
@@ -15,17 +15,22 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <unistd.h>
+
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
 #include <klocale.h>
+#include <ksplashscreen.h>
 
 #include <qtextcodec.h>
 
 #include "keurocalc.h"
 
-static const char *description =
-	I18N_NOOP("Cash converter and calculator for the Euro"),
-	*text = I18N_NOOP("A program by Éric Bischoff <e.bischoff@noos.fr>\n\nThis program is dedicated to Charles the Great,\nVictor Hugo, Homer, Giuseppe Verdi and all other\ngreat Europeans who didn't make it to have their\nfaces on the Euro banknotes.\n");
+static const char
+	*name =	I18N_NOOP("KEuroCalc"),
+	*description = I18N_NOOP("Cash converter and calculator"),
+	*copyright = I18N_NOOP("(c) 2001-2004, the KEuroCalc developers"),
+	*text = I18N_NOOP("A program by Éric Bischoff <e.bischoff@noos.fr>\n\nThis program is dedicated to humorist Michael Moore, for he\nknows that money does not justify anything...\n");
 
 // Command line arguments
 static KCmdLineOptions options[] =
@@ -35,20 +40,31 @@ static KCmdLineOptions options[] =
 
 int main(int argc, char *argv[])
 {
-	KAboutData aboutData( "keurocalc", I18N_NOOP("KEuroCalc"),
-	"0.9.0", description, KAboutData::License_GPL,
-	"(c) 2001-2004, the KEuroCalc developers", text);
+	KEuroCalc *keurocalc;
+	KAboutData aboutData( "keurocalc", name, "0.9.0", description,
+	 	              KAboutData::License_GPL, copyright, text);
+
 	aboutData.addAuthor("Éric Bischoff", I18N_NOOP("Design and implementation"), "e.bischoff@noos.fr");
 	aboutData.addCredit("Melchior Franz", I18N_NOOP("Design and testing"), "a8603365@unet.univie.ac.at");
+	aboutData.addCredit("Bas Willems", I18N_NOOP("Graphical artwork"), "cybersurfer@euronet.nl");
 	KCmdLineArgs::init( argc, argv, &aboutData );
 	KCmdLineArgs::addCmdLineOptions( options );
 
 	QTextCodec::setCodecForLocale( QTextCodec::codecForName("utf-8") );
-
 	KApplication a;
-	KEuroCalc *keurocalc = new KEuroCalc();
-	a.setMainWidget(keurocalc);
-	keurocalc->show();  
 
+	KSplashScreen *splash = new KSplashScreen( QPixmap( "splash.png" ) );
+	splash->message(i18n(description) + "\n" + i18n(copyright), Qt::AlignBottom);
+	splash->show();
+
+	keurocalc = new KEuroCalc();
+	a.setMainWidget(keurocalc);
+	keurocalc->show();
+
+	usleep(600000); // Something less idiotic to propose ?
+	splash->finish(keurocalc);
+	delete splash;
+
+	keurocalc->repaint();
 	return a.exec();
 }
