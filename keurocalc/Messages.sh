c81e0e3e23498ad9b40ecb7e3dc67ad3f3501cac
Éric Bischoff <bischoff@kde.org>
2010-05-15 08:42:10 +0000

    Resynchronizing with my own sources
    svn path=/trunk/extragear/utils/keurocalc/; revision=1126999

--- keurocalc/Messages.sh
+++ keurocalc/Messages.sh
@@ -1,7 +1,10 @@
-#! /usr/bin/env bash
+#! /bin/bash
 
 grep '[^-]name =' ../currencies/currencies.xml | sed 's/name = */I18N_NOOP(/; s/ *$$/);/' > names.cpp
 
-$XGETTEXT *.cpp -o $podir/keurocalc.pot
+uic-qt4 calculator.ui > calculator.cpp
+uic-qt4 settingsdialog.ui > settingsdialog.cpp
 
-rm names.cpp
+xgettext --from-code=utf-8 -ki18n -kki18n -kI18N_NOOP -ktranslate:2 *.cpp -o ../po/keurocalc.pot
+
+rm names.cpp calculator.cpp settingsdialog.cpp
