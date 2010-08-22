6963e2b456bc18cdfbeb44e7d3f5ffa0c328201e
Albert Astals Cid <aacid@kde.org>
2010-08-22 13:06:34 +0000

    fix extractrc calls
    svn path=/trunk/extragear/utils/keurocalc/; revision=1166676

--- keurocalc/Messages.sh
+++ keurocalc/Messages.sh
@@ -2,7 +2,7 @@
 
 grep '[^-]name =' ../currencies/currencies.xml | sed 's/name = */I18N_NOOP(/; s/ *$$/);/' > names.cpp
 
-$EXTRACTRC \*.ui >> rc.cpp
+$EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT *.cpp -o $podir/keurocalc.pot
 
 rm names.cpp
